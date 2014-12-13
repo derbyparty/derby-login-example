7c0796e815a36f94e7d14b75532143d7cb0497f0
Artur Zayats <zag2art@gmail.com>
2014-12-13 18:38:36 +0300

    add request hook

--- config/login.js
+++ config/login.js
@@ -120,6 +120,18 @@ module.exports = {
           }
           done(err);
       });
+    },
+    request: function(req, res, userId, isAuthenticated, done) {
+      // Redirect all unAuth GET requests to loginUrl
+      if (!isAuthenticated && req.method === 'GET' &&
+          req.url !== this.options.confirmRegistrationUrl &&
+          req.url !== this.options.loginUrl &&
+          req.url !== this.options.registrationConfirmedUrl &&
+          req.url.indexOf(this.options.recoverPasswordUrl) !== 0 &&
+          req.url.indexOf('/auth/') !== 0) {
+        return res.redirect(this.options.loginUrl);
+      }
+      done();
     }
   }
 }
